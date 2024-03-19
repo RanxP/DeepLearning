ba783f4f341a1b84ed61c586efc9328134741fc2
RanxP <78416756+RanxP@users.noreply.github.com>
2024-03-19 16:49:07 +0100

    removal of normalization

--- DataLoader.py
+++ DataLoader.py
@@ -9,30 +9,53 @@ from torchvision.datasets import Cityscapes
 import numpy as np
 from pathlib import Path
 
-CHANNEL_MEANS = [0.22460080459713935, 0.26541953831911086, 0.22537076537098202]
-CHANNEL_STDS = [0.019116874995935115, 0.02040196749932445, 0.02062898499852692]
+CHANNEL_MEANS = [0.485, 0.456, 0.406]
+CHANNEL_STDS = [0.229, 0.224, 0.225]
 
-IMG_SIZE = (512, 512)
-TRANSFORM_MASK =  transforms.Compose([
-    transforms.Resize(size=IMG_SIZE, interpolation=transforms.InterpolationMode.LANCZOS), # resize
-    # data transformation
-    transforms.PILToTensor(),
-    #.ConvertImageDtype(torch.float32),
-    ])
-TRANSFORM_IMAGE =  transforms.Compose([
-    transforms.Resize(size=IMG_SIZE,interpolation=transforms.InterpolationMode.LANCZOS), # resize
+IMG_SIZE = (512,1024)
+
+# ideas 5 crop
+# perspective transform
+#rotation 10 % each direction
+
+# color or gray scale transformations
+
+TRANSFORM_STRUCTURE = [
+    transforms.Resize(IMG_SIZE, interpolation=transforms.InterpolationMode.LANCZOS)
+    # transforms.RandomCrop(size=IMG_SIZE)
+    # transforms.RandomRotation(degrees=10),
+    ]
+
+TRANSFORM_IMAGE =  transforms.Compose(
+    TRANSFORM_STRUCTURE + [
     # data transformation
     transforms.PILToTensor(),
     transforms.ConvertImageDtype(torch.float32),
-    transforms.Normalize(mean=CHANNEL_MEANS, std = CHANNEL_STDS)
+    # transforms.Normalize(mean=CHANNEL_MEANS, std = CHANNEL_STDS)
     ])
 
+TRANSFORM_MASK =  transforms.Compose(
+    TRANSFORM_STRUCTURE + [
+    # data transformation
+    transforms.PILToTensor(),
+])
+
+# def transform_dual(instance):
+#     image, target = instance
+#     transform = RandomTransformsDual(TRANSFORM_STRUCTURE)
+#     image, target = transform(image, target)
+
+#     image = TRANSFORM_IMAGE(image)
+#     target = TRANSFORM_MASK(target)
+
+#     return image, target
 
 def generate_data_loaders(args) -> tuple[torch.utils.data.DataLoader, torch.utils.data.DataLoader, torch.utils.data.DataLoader]:
     
     trainset = Cityscapes(root = Path(args.data_path), split='train', mode='fine', 
-                          transform=TRANSFORM_IMAGE, target_transform = TRANSFORM_MASK,
-                          target_type='semantic')
+                      transform = TRANSFORM_IMAGE, target_transform=TRANSFORM_MASK,
+                      target_type='semantic')
+    
     train_subset, val_subset = torch.utils.data.random_split(trainset, [0.8, 0.2],
                                             generator=torch.Generator().manual_seed(1))
     
