370d84c32c10001d9223e249857261bbab554bb4
RanxP <78416756+RanxP@users.noreply.github.com>
2024-04-20 16:50:47 +0200

    6 decoders

--- ensamble/train.py
+++ ensamble/train.py
@@ -86,9 +86,9 @@ def main(args):
 
     # define model
     encoder = pre_trained_encoder()
-    encoder = load_model_weights(encoder, "model_final_ywahomd5.pth")
+    encoder = load_model_weights(encoder, "model_final_vhb12qyp.pth")
     
-    classes_to_ignore, decoders, optimizers = create_decoders(3)
+    classes_to_ignore, decoders, optimizers = create_decoders(6)
     model =  EnsambleModel(encoder, decoders)
     #load_encoder_weights(model, "model_final_vhb12qyp.pth")
     model.freeze_encoder()
